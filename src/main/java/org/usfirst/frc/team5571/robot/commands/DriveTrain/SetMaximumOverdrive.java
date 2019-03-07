10b17ed1583806c5e53e89636aa62a95887d3fb7
Zoarial94 <hunterw9469@gmail.com>
2019-03-07 11:24:20 -0500

    Speed Changing Commands are now instant commands

--- src/main/java/org/usfirst/frc/team5571/robot/commands/DriveTrain/SetMaximumOverdrive.java
+++ src/main/java/org/usfirst/frc/team5571/robot/commands/DriveTrain/SetMaximumOverdrive.java
@@ -3,38 +3,23 @@ package org.usfirst.frc.team5571.robot.commands.DriveTrain;
 import org.usfirst.frc.team5571.robot.Robot;
 import org.usfirst.frc.team5571.robot.Constants;
 
-import edu.wpi.first.wpilibj.command.Command;
+import edu.wpi.first.wpilibj.command.InstantCommand;
 
 /**
  *
  */
-public class SetMaximumOverdrive extends Command {
+public class SetMaximumOverdrive extends InstantCommand {
 
     public SetMaximumOverdrive() {
+        super();
         // Use requires() here to declare subsystem dependencies
         // eg. requires(chassis);
     }
 
     // Called just before this Command runs the first time
+    @Override
     protected void initialize() {
     	Robot.m_driveTrainSensitivity = Constants.driveTrainMaxSpeed;
     }
 
-    // Called repeatedly when this Command is scheduled to run
-    protected void execute() {
-    }
-
-    // Make this return true when this Command no longer needs to run execute()
-    protected boolean isFinished() {
-        return true;
-    }
-
-    // Called once after isFinished returns true
-    protected void end() {
-    }
-
-    // Called when another command which requires one or more of the same
-    // subsystems is scheduled to run
-    protected void interrupted() {
-    }
 }
